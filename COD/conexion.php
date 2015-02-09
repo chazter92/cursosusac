f4df2688616ac4450a2bcfeb006b0d5eb4ac47b2
Mario14264 <mariorodas92@gmail.com>
2015-02-09 00:17:39 -0600

    var nueva
    prueba para jenkins

--- COD/conexion.php
+++ COD/conexion.php
@@ -1,6 +1,7 @@
 <?php
 function conectar () {
 $db_con = mysql_connect (DB_SERVER,DB_USER,DB_PASS);
+$varPrueba = 5; // prueba para jenkins
 if (!$db_con) return false;
 if (!mysql_select_db (DB_NAME, $db_con)) return false;
 mysql_set_charset('utf8'); 
