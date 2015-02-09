e44623dce20355d58f21b58628a6369204fdd427
rguaran <ritaguaran@gmail.com>
2015-02-09 12:59:13 -0600

    test
    Conflicts:
    	COD/conexion.php
    
    prueba

--- COD/conexion.php
+++ COD/conexion.php
@@ -1,7 +1,8 @@
 <?php
 function conectar () {
 $db_con = mysql_connect (DB_SERVER,DB_USER,DB_PASS);
-$hola = 58// prueba para jenkins
+$hola = 58;// prueba para jenkins
+
 if (!$db_con) return false;
 if (!mysql_select_db (DB_NAME, $db_con)) return false;
 mysql_set_charset('utf8'); 
