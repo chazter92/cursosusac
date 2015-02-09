7d3d393ae31431a405f1d5c7398b42d791338a91
Mario14264 <mariorodas92@gmail.com>
2015-02-09 12:49:58 -0600

    prueba error 2
    var hola con valor

--- COD/conexion.php
+++ COD/conexion.php
@@ -1,7 +1,7 @@
 <?php
 function conectar () {
 $db_con = mysql_connect (DB_SERVER,DB_USER,DB_PASS);
-$hola =// prueba para jenkins
+$hola = 58// prueba para jenkins
 if (!$db_con) return false;
 if (!mysql_select_db (DB_NAME, $db_con)) return false;
 mysql_set_charset('utf8'); 
