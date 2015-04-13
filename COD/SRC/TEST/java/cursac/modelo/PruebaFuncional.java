f03cd360ba32bc1def3c7fb8553ebea5b66e6de7
Mario14264 <mariorodas92@gmail.com>
2015-04-13 15:44:13 -0600

    prueba funcional

---
+++ COD/SRC/TEST/java/cursac/modelo/PruebaFuncional.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursac.modelo;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.SeleneseTestBase;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author Mario Rodas
+ */
+public class PruebaFuncional extends SeleneseTestBase {
+    
+    @Before
+    public void beforeClass() {
+        selenium = new DefaultSelenium("localhost", 4444, "*firefox", "http://localhost:8098");
+        selenium.start();        
+    }
+    
+    @Test
+    public void testSimple() throws Exception {
+        selenium.open("/cursac/consultarCurso.jsp");
+        selenium.waitForPageToLoad("50000");
+        selenium.type("id=txtCodigo", "101");
+        selenium.click("id=btnPrePost");
+        selenium.waitForPageToLoad("50000");
+    }
+    
+}
